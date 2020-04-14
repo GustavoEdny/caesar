296c7ffa19425ea20003204d4da27ef1d17790f2
Gustavo Edny <gustavoedny@gmail.com>
2020-04-14 11:43:01 -0300

    Fixing tests
    -  Changing the encrypt behavior to increase instead decrease a letter
    - adding decrypt method tests

--- test/caesar/cipher_test.exs
+++ test/caesar/cipher_test.exs
@@ -5,18 +5,36 @@ defmodule Caesar.CipherTest do
   import Caesar.Cipher
 
   test "encrypt shift cipher mapping" do
-    assert encrypt("abcd", 1) == "zabc"
+    assert encrypt("abcd", 1) == "bcde"
   end
 
   test "encrypt handles capital letters" do
-    assert encrypt("abCD", 1) == "zaBC"
+    assert encrypt("abCD", 1) == "bcDE"
   end
 
-  test "encrypt handles spaces" do
-    assert encrypt("ab cd", 1) == "za bc"
+  test "encrypt handles spaces and special characters" do
+    assert encrypt("ab cd", 1) == "bc de"
+    assert encrypt("Hello :)", 1) == "Ifmmp :)"
   end
 
   test "encrypt handles a large shift number" do
-    assert encrypt("abcd", 27) == "zabc"
+    assert encrypt("abcd", 27) == "bcde"
+  end
+
+  test "decrypt shift cipher mapping" do
+    assert decrypt("bcde", 1) == "abcd"
+  end
+
+  test "decrypt handles capital letters" do
+    assert decrypt("bcDE", 1) == "abCD"
+  end
+
+  test "decrypt handles spaces and special characters" do
+    assert decrypt("bc de", 1) == "ab cd"
+    assert decrypt("Ifmmp :)", 1) == "Hello :)"
+  end
+
+  test "decrypt handles a large shift number" do
+    assert decrypt("bcde", 27) == "abcd"
   end
 end
